511340ba76b39871e23c2fddc6ed6e3ff17a17e3
ursnaksh <er.nageshs@gmail.com>
2026-09-18 00:15:27 +0530

    fix: preserve env file encoding

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -56,6 +56,7 @@ class Settings(BaseSettings):
     else:
         class Config:
             env_file = ".env"
+            env_file_encoding = "utf-8"
             case_sensitive = True
             extra = "ignore"
 
